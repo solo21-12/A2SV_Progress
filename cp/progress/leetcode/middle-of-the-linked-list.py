bf79cb10989eafc4cdee582261028b32315838da
Dawit Abraham Beyene <93918988+solo21-12@users.noreply.github.com>
2024-02-11 23:39:35 +0300

    Add solution for Middle of the Linked List

--- cp/progress/leetcode/middle-of-the-linked-list.py
+++ cp/progress/leetcode/middle-of-the-linked-list.py
@@ -4,22 +4,11 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def get_length(self, head: Optional[ListNode]):
-        count = 0
-        itr = head
-
-        while itr:
-            count += 1
-            itr = itr.next
-
-        return count // 2
-
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        count = self.get_length(head)
+        slow,fast = head,head
 
-        itr = head
-        while count:
-            itr = itr.next
-            count -= 1
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
 
-        return itr
+        return slow
