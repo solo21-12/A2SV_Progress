cdad9a64670ef03f84323f17c0f90bc7d2dbe2ac
Dawit Abraham Beyene <93918988+solo21-12@users.noreply.github.com>
2024-02-12 16:44:41 +0300

    Add solution for Linked List Cycle II

---
+++ cp/progress/leetcode/linked-list-cycle-ii.py
@@ -0,0 +1,33 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+
+class Solution:
+    def intersection(self, head):
+        slow, fast = head, head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast:
+                return slow
+
+        return None
+
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        intersection = self.intersection(head)
+
+        if head == None or intersection == None:
+            return None
+
+        start = head
+
+        while start != intersection:
+            start = start.next
+            intersection = intersection.next
+
+        return start
