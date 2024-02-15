01f2f6794238dc0aa7c6828563ea9c6bd4eaacd5
Dawit Abraham Beyene <93918988+solo21-12@users.noreply.github.com>
2024-02-15 13:57:19 +0300

    Add solution for Sort Colors

---
+++ cp/progress/leetcode/sort-colors.py
@@ -0,0 +1,19 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        low,mid,high=0,0,len(nums) - 1
+        while mid <= high:
+            if nums[mid] == 0:
+                nums[low],nums[mid] = nums[mid],nums[low]
+                low += 1
+                mid += 1
+            elif nums[mid] == 2:
+                nums[high],nums[mid] = nums[mid],nums[high]
+                high -= 1
+            else:
+                mid += 1
+
+
+        
