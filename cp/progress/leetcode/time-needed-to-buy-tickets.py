7b5547b996c216ead7a7df702aa746782b9d2b33
Dawit Abraham Beyene <93918988+solo21-12@users.noreply.github.com>
2024-02-19 13:09:37 +0300

    Add solution for Time Needed to Buy Tickets

--- cp/progress/leetcode/time-needed-to-buy-tickets.py
+++ cp/progress/leetcode/time-needed-to-buy-tickets.py
@@ -1,24 +1,18 @@
 class Solution:
     def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
-        queue = deque()
-        for i, ticket in enumerate(tickets):
-            queue.append((ticket, i))
+        queue = deque([(ticket, i) for i, ticket in enumerate(tickets)] )
 
         count = 0
         while True:
             count += 1
             ticket, i = queue.popleft()
             ticket -= 1
-            print(ticket, i)
 
-            if ticket > 0:
-                queue.append((ticket, i))
-            
             if i == k and not ticket:
                 return count
 
+            if ticket > 0:
+                queue.append((ticket, i))
+
+
         return 0
-
-
-
-        
