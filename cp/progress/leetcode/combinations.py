ba8b2da0947d1e357e68b0c30c1df94b0501e742
Dawit Abraham Beyene <93918988+solo21-12@users.noreply.github.com>
2024-02-26 19:08:12 +0300

    Add solution for Combinations

---
+++ cp/progress/leetcode/combinations.py
@@ -0,0 +1,43 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+
+        def combinations(i, arr):
+            # This implmentation has a slower time due to a second recursion
+            if i > n:
+                return
+            
+            if len(arr) == k:
+                ans.append(arr[:])
+                return
+
+            # This is picking the current index element
+            arr.append(i + 1)
+            combinations(i + 1, arr)
+
+            # This is not picking the current element and move to the next index element
+            arr.pop()
+            combinations(i + 1, arr)
+
+
+        def backtrack(i, arr):
+            print(arr)
+            if len(arr) == k:
+                ans.append(arr[:])
+                return
+
+            if arr and arr[-1] > n - 1:
+                return
+
+            for choice in range(i + 1, n + 1):
+                arr.append(choice)
+                backtrack(choice, arr)
+                arr.pop()
+
+
+        ans = []
+        # combinations(0, [])
+        backtrack(0, [])
+
+        return ans
+
+
