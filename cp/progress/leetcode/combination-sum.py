1235fa9e5d4b5a095d6046eec4bc3fa75a2352aa
Dawit Abraham Beyene <93918988+solo21-12@users.noreply.github.com>
2024-02-26 19:09:22 +0300

    Add solution for Combination Sum

--- cp/progress/leetcode/combination-sum.py
+++ cp/progress/leetcode/combination-sum.py
@@ -11,6 +11,7 @@ class Solution:
                     ans.append(arr[:])
                 return
 
+            # if at any stage we reach to the target 
             if not target:
                 ans.append(arr[:])
                 return
@@ -19,17 +20,19 @@ class Solution:
             if target >= candidates[i]:
                 arr.append(candidates[i])
                 findSubSequence(i, target - candidates[i], arr)
+                # This is removing the picked element
                 arr.pop()
-
-                
 
             # This is when not picking
             findSubSequence(i + 1, target, arr)
-           
 
-        
         findSubSequence(0, target, [])
 
         return ans
-            
 
+
+'''
+the idea is as follows if the current element is less than the target we keep picking the element 
+else we remove it and go to the next index
+
+'''
