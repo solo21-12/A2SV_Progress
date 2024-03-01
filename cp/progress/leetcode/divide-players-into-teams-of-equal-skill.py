f096f82d195dbf3c8354a98f8c8330a525f27c62
Dawit Abraham Beyene <93918988+solo21-12@users.noreply.github.com>
2024-03-01 07:11:23 +0300

    Add solution for Divide Players Into Teams of Equal Skill

--- cp/progress/leetcode/divide-players-into-teams-of-equal-skill.py
+++ cp/progress/leetcode/divide-players-into-teams-of-equal-skill.py
@@ -2,19 +2,16 @@ class Solution:
     def dividePlayers(self, skill: List[int]) -> int:
         skill.sort()
 
-        l,r = 1, len(skill) - 2
+        l, r = 1, len(skill) - 2
         minSum = skill[0] + skill[-1]
-        # print(minSum)
 
         ans = skill[0] * skill[-1]
         while l < r:
-            print()
             if skill[l] + skill[r] != minSum:
                 return -1
-            
-            ans += (skill[l] * skill[r])
+
+            ans += skill[l] * skill[r]
             l += 1
             r -= 1
 
         return ans
-        
