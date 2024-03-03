09d0f8f134f1301b8745109de33bbce8d765f3b3
Dawit Abraham Beyene <93918988+solo21-12@users.noreply.github.com>
2024-03-03 18:22:30 +0300

    Add solution for Partition Labels

--- cp/progress/leetcode/partition-labels.py
+++ cp/progress/leetcode/partition-labels.py
@@ -20,8 +20,42 @@ class Solution:
 
         # return res
 
-        intervals = []
-        interval_map ={}
+        # intervals = []
+        # interval_map ={}
+        # ans = []
+
+        # for i in range(len(s)):
+        #     if s[i] not in interval_map:
+        #         interval_map[s[i]] = [i,i]
+        #     else:
+        #         interval_map[s[i]][1] = i
+        
+        # for interval in interval_map.values():
+        #     intervals.append(interval)
+        
+        # intervals.sort(key=lambda x:x[0])
+        # ans.append(intervals[0])
+
+        
+        # for i in range(1,len(intervals)):
+        #     lastEnd = ans[-1][1]
+
+        #     if intervals[i][0] <= lastEnd:
+        #         ans[-1][1] = max(intervals[i][1],lastEnd)
+
+        #     else:
+        #         ans.append(intervals[i])
+
+
+                
+        # res = []
+        # for start,end in ans:
+        #     res.append(end - start + 1)
+
+        # return res
+
+
+        interval_map = {}
         ans = []
 
         for i in range(len(s)):
@@ -29,27 +63,13 @@ class Solution:
                 interval_map[s[i]] = [i,i]
             else:
                 interval_map[s[i]][1] = i
+        end = 0
+        start = -1
+        for i in range(len(s)):
+            end = max(interval_map[s[i]][1], end)
+
+            if i == end:
+                ans.append(end - start)
+                start = i
         
-        for interval in interval_map.values():
-            intervals.append(interval)
-        
-        intervals.sort(key=lambda x:x[0])
-        ans.append(intervals[0])
-
-        
-        for i in range(1,len(intervals)):
-            lastEnd = ans[-1][1]
-
-            if intervals[i][0] <= lastEnd:
-                ans[-1][1] = max(intervals[i][1],lastEnd)
-
-            else:
-                ans.append(intervals[i])
-
-
-                
-        res = []
-        for start,end in ans:
-            res.append(end - start + 1)
-
-        return res
+        return ans
