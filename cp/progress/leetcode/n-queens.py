1698cc2e9d0fb986ff92bcf5662637f1d87d4f0a
Dawit Abraham Beyene <93918988+solo21-12@users.noreply.github.com>
2024-03-06 13:03:42 +0300

    Add solution for N-Queens

--- cp/progress/leetcode/n-queens.py
+++ cp/progress/leetcode/n-queens.py
@@ -1,44 +1,35 @@
-from typing import List
-
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
 
-        def not_same_diagonal(coords):
-            n = len(coords)
-            for i in range(n - 1):
-                for j in range(i + 1, n):
-                    x1, y1 = coords[i]
-                    x2, y2 = coords[j]
-                    if abs(x1 - x2) == abs(y1 - y2):
-                        return False
-            return True
-
-        def backtrack(row, path, col):
+        
+        def backtrack(row, path, col, posDeg, negDeg):
             nonlocal ans
-            if not not_same_diagonal(path):
-                return
 
-            if row == n:
+            if len(path) == n:
                 ans.append(path[:])
                 return
 
             for c in range(n):
-                if c not in col:
+                if c not in col and (row + c) not in posDeg and (row - c) not in negDeg :
                     path.append((row, c))
                     col.add(c)
-                    backtrack(row + 1, path, col)
+                    posDeg.add(row + c)
+                    negDeg.add(row - c)
+                    backtrack(row + 1, path, col, posDeg, negDeg)
                     path.pop()
                     col.remove(c)
+                    posDeg.remove(row + c)
+                    negDeg.remove(row - c)
 
         ans = []
-        backtrack(0, [], set())
+        backtrack(0, [], set(), set(), set())
+        print(ans)
 
         result = []
         for num in ans:
             solution = ["." * n for _ in range(n)]
             for r, c in num:
                 solution[r] = solution[r][:c] + "Q" + solution[r][c + 1:]
-                
             result.append(solution)
 
         return result
